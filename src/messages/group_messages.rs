ecd9ac1d55f6ce8816643afed9006c108d0902c5
Borja Aranda <borja@ephemerahq.com>
2025-02-07 12:12:45 +0100

    handle bytes correctly

--- src/messages/group_messages.rs
+++ src/messages/group_messages.rs
@@ -5,42 +5,39 @@
 use alloc::vec::Vec;
 
 use alloy_primitives::{U256, FixedBytes, Bytes};
+use alloy_sol_types::sol;
 use stylus_sdk::{
+    abi::Bytes as AbiBytes,
     evm,
-    prelude::{storage, entrypoint},
-    storage::StorageU256,
+    prelude::*,
     stylus_proc::public,
 };
-use alloy_sol_types::sol;
 
 sol! {
     #[allow(missing_docs)]
     event MessageSent(bytes32 groupId, bytes message, uint256 sequenceId);
 }
 
-
-/// State of a [`GroupMessages`] Contract.
-#[entrypoint]
-#[storage]
-pub struct GroupMessages {
-    pub sequence_id: StorageU256
+sol_storage! {
+    #[entrypoint]
+    pub struct GroupMessages {
+        uint256 sequence_id;
+    }
 }
-
 
 #[public]
 impl GroupMessages {
     /// Stores a new MLS commit message.
     /// This replicates `addMessage(bytes32 groupId, bytes message)` from the Solidity contract.
     /// Emits a `MessageSent` log with `(groupId, message, sequenceId)`
-    pub fn add_message(&mut self, group_id: FixedBytes<32>, message: Vec<u8>) {
-        let current_seq = self.sequence_id.get();
-        let new_seq = current_seq + U256::from(1);
-        self.sequence_id.set(new_seq);
+    pub fn add_message(&mut self, group_id: FixedBytes<32>, message: AbiBytes) {
+        let sequence_id = self.sequence_id.get();
+        self.sequence_id.set(sequence_id + U256::from(1));
 
         evm::log(MessageSent {
             groupId: group_id,
-            message: Bytes::from(message),
-            sequenceId: new_seq,
+            message: Bytes::from(message.to_vec()),
+            sequenceId: self.sequence_id.get(),
         });
     }
 }
